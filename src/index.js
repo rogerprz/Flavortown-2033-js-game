65a906634a4cb13a2dd478e6b2265ac778ef3b11
briecodes <hello@brie.work>
2018-05-29 17:11:52 -0400

    collision editing.

--- src/index.js
+++ src/index.js
@@ -16,24 +16,37 @@ var gameInterval = null
 
 
 function checkCollision(rock) {
-  const top = positionToInteger(rock.style.top)
+  const rockTop = positionToInteger(rock.style.top);
+  const rockLeftEdge = positionToInteger(rock.style.left);
+  const rockRightEdge = positionToInteger(rock.style.left) + 20;
+
+  const dodgerTop = positionToInteger(DODGER.style.top);
+  const dodgerLeftEdge = positionToInteger(DODGER.style.left);
+  const dodgerRightEdge = positionToInteger(DODGER.style.left) + 40;
+
+  if (rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge || rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge || rockLeftEdge <= dodgerRightEdge && rockRightEdge >= dodgerRightEdge
+  ) {
+    return true
+  }
+
   // rocks are 20px high
   // DODGER is 20px high
   // GAME_HEIGHT - 20 - 20 = 360px;
-  if (top > 360) {
-    const dodgerLeftEdge = positionToInteger(DODGER.style.left)
-    // FIXME: The DODGER is 40 pixels wide -- how do we get the right edge?
-    const dodgerRightEdge = positionToInteger(DODGER.style.left) + 40
 
-    const rockLeftEdge = positionToInteger(rock.style.left)
-    // FIXME: The rock is 20 pixel's wide -- how do we get the right edge?
-    const rockRightEdge = positionToInteger(rock.style.left) + 20
-    if (rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge || rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge || rockLeftEdge <= dodgerRightEdge && rockRightEdge >= dodgerRightEdge
-    ) {
-      return true
+  // if (top > 360) {
+  //   const dodgerLeftEdge = positionToInteger(DODGER.style.left)
+  //   // FIXME: The DODGER is 40 pixels wide -- how do we get the right edge?
+  //   const dodgerRightEdge = positionToInteger(DODGER.style.left) + 40
 
-    }
-  }
+  //   const rockLeftEdge = positionToInteger(rock.style.left)
+  //   // FIXME: The rock is 20 pixel's wide -- how do we get the right edge?
+  //   const rockRightEdge = positionToInteger(rock.style.left) + 20
+  //   if (rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge || rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge || rockLeftEdge <= dodgerRightEdge && rockRightEdge >= dodgerRightEdge
+  //   ) {
+  //     return true
+
+  //   }
+  // }
 }
 
 
