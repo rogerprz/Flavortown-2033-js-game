f95a1a8097c9a7ae3a0ff255936e459950713f9d
rogercodes1 <roger.ucsb@gmail.com>
2018-05-29 17:43:23 -0400

    removed comments that were repeated.

--- src/index.js
+++ src/index.js
@@ -33,29 +33,11 @@ function checkCollision(rock) {
   const dodgerLeftEdge = positionToInteger(DODGER.style.left);
   const dodgerRightEdge = positionToInteger(DODGER.style.left) + 40;
 
-  if (rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge || rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge || rockLeftEdge <= dodgerRightEdge && rockRightEdge >= dodgerRightEdge
-  ) {
+  if (rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge || rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge || rockLeftEdge <= dodgerRightEdge && rockRightEdge >= dodgerRightEdge) {
     return true
   }
 
-  // rocks are 20px high
-  // DODGER is 20px high
-  // GAME_HEIGHT - 20 - 20 = 360px;
 
-  // if (top > 360) {
-  //   const dodgerLeftEdge = positionToInteger(DODGER.style.left)
-  //   // FIXME: The DODGER is 40 pixels wide -- how do we get the right edge?
-  //   const dodgerRightEdge = positionToInteger(DODGER.style.left) + 40
-
-  //   const rockLeftEdge = positionToInteger(rock.style.left)
-  //   // FIXME: The rock is 20 pixel's wide -- how do we get the right edge?
-  //   const rockRightEdge = positionToInteger(rock.style.left) + 20
-  //   if (rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge || rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge || rockLeftEdge <= dodgerRightEdge && rockRightEdge >= dodgerRightEdge
-  //   ) {
-  //     return true
-
-  //   }
-  // }
 }
 
 
