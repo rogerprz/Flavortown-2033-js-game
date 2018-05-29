911f254c2257c8a8ed09244eb693cb5faa2770cf
github username <jrgreiner115@gmail.com>
2018-05-29 13:56:47 -0400

    space event

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 let ground = document.getElementById("ground")
 
 document.addEventListener('DOMContentLoaded', function(e) {
+  console.log("DOM Loaded!");
+  avatar = document.createElement("div")
+  avatar.style = "#avatar"
+
+
+  document.addEventListener("keypress", (e) => {
+    debugger
+    if (e.code === "Space") {
+      
+    }
+
+
+  })
 
 });
 
