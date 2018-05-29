8fa4ae6c455a53c2450dca4f3e18e806a7704324
rogercodes1 <roger.ucsb@gmail.com>
2018-05-29 14:32:05 -0400

    up n down working

--- src/index.js
+++ src/index.js
@@ -1,9 +1,68 @@
 let ground = document.getElementById("ground")
 
 document.addEventListener('DOMContentLoaded', function(e) {
-
+  startGame()
 });
 
-function styles() {
+
+var myGamePiece;
+
+function startGame() {
+  myGamePiece = new component(80, 50, "red", 10, 120);
+  gameArea.start();
+}
+
+
+
+
+
+var gameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = window.innerWidth-100;
+        this.canvas.height = window.innerHeight-100;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 20);
+    },
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+}
+
+function component(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.x = x;
+    this.y = y;
+    this.update = function() {
+        ctx = gameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+    this.newPos = function() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }
+}
+
+function updateGameArea() {
+    gameArea.clear();
+    myGamePiece.newPos();
+    myGamePiece.update();
+}
+
+
+function moveShip() {
+  if (event.keyCode === 38){
+    myGamePiece.speedY -= 1;
+  }
+  if (event.keyCode === 40){
+    myGamePiece.speedY += 1;
+  }
+
+
 
 }
